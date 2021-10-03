3f937e45c9688825794058341e2716df92375f96
Aurelio Márquez <aurelio3927@gmail.com>
2021-10-03 16:04:58 -0400

    Fisrt commit

---
+++ src/main/java/com/malm/atos/vrental/VrentalApplication.java
@@ -0,0 +1,13 @@
+package com.malm.atos.vrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malm/atos/vrental/VrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malm.atos.vrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
